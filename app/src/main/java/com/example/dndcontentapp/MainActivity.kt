b237e6c377aa434d333d639d89ba81b9161aecd9
CameronWNelson <49245815+CameronWNelson@users.noreply.github.com>
2024-04-08 22:01:04 -0700

    Add basic search bar, need input sanitization.
    Lacking input sanitization and list doesn't refresh when search query is emptied.

--- app/src/main/java/com/example/dndcontentapp/MainActivity.kt
+++ app/src/main/java/com/example/dndcontentapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.dndcontentapp
 
+import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -7,26 +8,53 @@ import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var spellRecyclerView : RecyclerView
+    private lateinit var searchView : androidx.appcompat.widget.SearchView
     private lateinit var allSpells : ArrayList<SpellData>
+    private lateinit var currentSpells : ArrayList<SpellData>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val dbManager = getDbManager(this)
         val database = dbManager.openDatabase()
-        allSpells = ArrayList<SpellData>()
+        allSpells = ArrayList()
 
-        initializeSpellData(allSpells)
+        initializeAllSpellData(allSpells)
+        currentSpells = allSpells
 
         spellRecyclerView = findViewById(R.id.SpellRecyclerView)
         spellRecyclerView.layoutManager = LinearLayoutManager(this)
-        spellRecyclerView.adapter = SpellRecyclerViewAdapter(allSpells)
+        spellRecyclerView.adapter = SpellRecyclerViewAdapter(currentSpells)
+
+        searchView = findViewById(R.id.SearchView)
+        searchView.setOnQueryTextListener(object : androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != "") {
+                    val cursor = dbManager.readableDatabase.rawQuery("SELECT * FROM spells WHERE name LIKE \'%$query%\'", null)
+                    currentSpells.clear()
+                    initializeSpellData(currentSpells, cursor)
+                }
+                else {
+                    currentSpells = allSpells
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return false
+            }
+        })
     }
 
     // create a list of all the spells in the database
-    private fun initializeSpellData(spells: ArrayList<SpellData>) {
-        var spellCursor = dbManager.readableDatabase.rawQuery("SELECT * FROM spells", null)
-        while (spellCursor.moveToNext()) {
-            spells.add(SpellData.createFromCursor(spellCursor))
+    private fun initializeAllSpellData(spells: ArrayList<SpellData>) {
+        val cursor = dbManager.readableDatabase.rawQuery("SELECT * FROM spells", null)
+        initializeSpellData(spells, cursor)
+    }
+
+    private fun initializeSpellData(spells: ArrayList<SpellData>, cursor: Cursor) {
+        while (cursor.moveToNext()) {
+            spells.add(SpellData.createFromCursor(cursor))
         }
     }
 }
